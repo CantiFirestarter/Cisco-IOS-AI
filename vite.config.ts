ae63e60c5cd6e79fa309220c48c3e7168584f86c
Canti Firestarter <lordcantix@gmail.com>
2026-01-01 05:31:47 -0600

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,10 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   define: {
     // This ensures process.env.API_KEY works in the browser after building
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
+    'process.env.API_KEY': JSON.stringify(process.env.VITE_API_KEY || process.env.API_KEY),
   },
   build: {
     outDir: 'dist',
-  }
+  },
+  base: './',
 });
